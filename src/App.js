d883173dadccaf833a3e067cfff1c88524f25b4b
Kirill Ginko <kirillginko@gmail.com>
2021-08-22 13:37:07 -0400

    added meta and favicon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import "./styles/app.scss";
 //Import Components
 import Player from "./components/Player";
